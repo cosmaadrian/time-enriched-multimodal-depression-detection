403d1f2990a987086483eec6807ee883741923a5
Ana <bucuram@ymail.com>
2024-01-17 10:40:47 +0200

    updated split_reddit_shards.py

--- scripts/split_reddit_shards.py
+++ scripts/split_reddit_shards.py
@@ -66,8 +66,6 @@ users = df["user"].tolist()
 
 for i, user in enumerate(sorted(users)):
     paths_df = {}
-    if "itchyyyyscrotum" in user:
-        continue
 
     user_df = pd.read_csv(
         f"{SPLITS_PATH}/for_experiments/{KIND}/{user}.csv", lineterminator="\n"
